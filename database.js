6e50fb09bac6599586b65ce7717187d72f81f487
Brock Klein <bcklein1@gmail.com>
2020-03-19 09:33:50 -0400

    completed database

---
+++ database.js
@@ -0,0 +1,94 @@
+var Sequelize = require('sequelize')
+
+const seqDbCreate = new Sequelize('', 'root', 'M1tim6!17b', {
+	dialect: 'mariadb',
+	port: 3306
+});
+
+const seqDb = new Sequelize('bnbReviews', 'root', 'M1tim6!17b', {
+	dialect: 'mariadb',
+	port: 3306
+})
+
+var User = seqDb.define('user', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	profilePicUrl: Sequelize.STRING,
+	firstName: Sequelize.STRING,
+	lastName: Sequelize.STRING
+});
+
+var Listing = seqDb.define('listing', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	listingTitle: Sequelize.STRING
+});
+
+var Review = seqDb.define('review', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	createdBy: {
+		type: Sequelize.INTEGER,
+		references: { model: User, key: 'id' }
+	},
+	listing: {
+		type: Sequelize.INTEGER,
+		references: { model: Listing, key: 'id' }
+	},
+	ratingCheckIn: Sequelize.INTEGER,
+	ratingAccuracy: Sequelize.INTEGER,
+	ratingCleanliness: Sequelize.INTEGER,
+	ratingCommunication: Sequelize.INTEGER,
+	ratingLocation: Sequelize.INTEGER,
+	ratingValue: Sequelize.INTEGER,
+	reviewText: Sequelize.STRING,
+	date: Sequelize.DATE
+});
+
+var OwnerReply = seqDb.define('ownerReply', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	createdBy: {
+		type: Sequelize.INTEGER,
+		references: { model: User, key: 'id' }
+	},
+	reviewId: {
+		type: Sequelize.INTEGER,
+		references: { model: Review, key: 'id' }
+	},
+	replyText: Sequelize.STRING,
+	date: Sequelize.DATE
+});
+
+seqDbCreate
+	.authenticate()
+	.then(() => {
+		console.log('seqDbCreate connection has been established.')
+		return seqDbCreate.query('DROP DATABASE IF EXISTS bnbReviews')
+	})
+	.then(() => {
+		return seqDbCreate.query('CREATE DATABASE IF NOT EXISTS bnbReviews')
+	})
+	.then(() => {
+		return seqDb.authenticate()
+	})
+	.then(() => {
+		seqDb.sync({ force: true })
+	})
+	.catch((err) => console.log('Error initializing- ', err))
+
+
+
+
